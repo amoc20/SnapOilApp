f9fb9fe7b1c6077e69d7b1c859afd6a7f0865be0
amoc20 <moc.ales@seznam.cz>
2023-03-31 12:36:15 +0200

    Main activity, added styles

--- app/src/main/java/com/example/snapoil/view/MainActivity.kt
+++ app/src/main/java/com/example/snapoil/view/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.snapoil.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.snapoil.R
+import com.example.snapoil.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startBtn.setOnClickListener {
+            //startActivity(Intent(this, SelectBrandActivity::class.java))
+        }
+
+        binding.endBtn.setOnClickListener {
+            val intent = Intent(Intent.ACTION_MAIN)
+            intent.addCategory(Intent.CATEGORY_HOME)
+            startActivity(intent)
+        }
     }
 }
